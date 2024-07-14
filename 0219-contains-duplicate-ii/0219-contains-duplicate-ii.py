422d5c07dc06d461f6671cbfc3c24e736e40a508
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-07-14 19:47:40 +0530

    Time: 474 ms (53.42%), Space: 22.2 MB (79.16%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        d = {}
+
+        for i, n in enumerate(nums):
+          if n in d and abs(i - d[n]) <= k:
+            return True
+          else:
+            d[n] = i
+        
+        return False
