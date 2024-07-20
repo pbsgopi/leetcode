61215e5682bca604c011a5b11437741212604f17
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-07-20 11:50:50 +0530

    Time: 683 ms (43.48%), Space: 16.4 MB (47.83%) - LeetHub

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def restoreMatrix(self, rowSum, colSum):
+        """
+        :type rowSum: List[int]
+        :type colSum: List[int]
+        :rtype: List[List[int]]
+        """
+        m, n = len(rowSum), len(colSum)
+        ans = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                x = min(rowSum[i], colSum[j])
+                ans[i][j] = x
+                rowSum[i] -= x
+                colSum[j] -= x
+        return ans
