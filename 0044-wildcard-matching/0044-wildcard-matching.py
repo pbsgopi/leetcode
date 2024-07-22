9609b1270f8d2f9cd7cafde1254ffd0563587640
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-07-22 16:16:24 +0530

    Time: 522 ms (17.53%), Space: 23.8 MB (72.01%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False]*(len(p)+1) for i in range(len(s)+1)]
+        dp[0][0] = True
+        for j in range(1,len(p)+1):
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-1]
+        
+        for i in range(1,len(s)+1):
+            for j in range(1,len(p)+1):
+                dp[i][j] = (p[j-1] in [s[i-1],'?','*'] and dp[i-1][j-1]) or (p[j-1] == '*' and (dp[i][j-1] or dp[i-1][j]))
+  
+        return dp[len(s)][len(p)]
