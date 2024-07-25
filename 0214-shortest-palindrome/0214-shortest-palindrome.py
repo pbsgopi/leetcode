1e938ce037f8da7c9c6bf1d05b687ee452e316eb
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-07-25 13:18:18 +0530

    Time: 62 ms (72.09%), Space: 16.9 MB (55.71%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        t = s[::-1]
+        for i in range(len(t)):
+            if s.startswith(t[i:]):
+                return t[:i] + s
+        return t+s
