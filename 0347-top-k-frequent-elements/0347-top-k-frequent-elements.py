29a235150da5ec155b8dc9075a128a0868462c5c
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-01 21:44:22 +0530

    Time: 93 ms (36%), Space: 21.2 MB (41.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_table = Counter(nums)
-        heap = []
-        for i in freq_table.keys():
-            heappush(heap, (freq_table[i], i))
-        freq_table = nlargest(k,heap)
-        ans = []
-        for i, j in freq_table:
-            ans.append(j)
-        return ans
+        counter = Counter(nums)
+        top = counter.most_common(k)
+        return [key for key, count in top]
