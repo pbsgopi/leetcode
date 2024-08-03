6e30742837b549032001a10b7438815ed6f949f8
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-03 22:59:16 +0530

    Time: 35 ms (76.46%), Space: 16.6 MB (6.63%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -1,10 +1,7 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
         n= len(nums)
-        count=0
         for i in range(n):
-            if nums[(i + 1) % n] < nums[i]:
-                count += 1
-            if count > 1:
-                return False  
-        return True
+            if nums[i:]+nums[:i]==sorted(nums):
+                return True
+        return False
