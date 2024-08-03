dd88fdc690406340e45a4619eadf2e264cb7cc41
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-03 11:37:58 +0530

    Time: 38 ms (50.72%), Space: 16.7 MB (6.4%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,9 +1,21 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        hset = set()
-        while n != 1:
-            if n in hset: return False
-            hset.add(n)
-            n = sum([int(i) ** 2 for i in str(n)])
-        else:
+        slow, fast = n, n
+        slow = self.sumsqr(slow)
+        fast = self.sumsqr(self.sumsqr(fast))
+        if slow==1 or fast==1:
             return True
+
+        while slow!=fast:
+            if fast == 1:
+                return True
+            slow = self.sumsqr(slow)
+            fast = self.sumsqr(self.sumsqr(fast))
+        return False    
+    def sumsqr(self, n):
+        sum = 0
+        while n>0:
+            ld = n%10
+            sum += ld**2
+            n = n//10
+        return sum
