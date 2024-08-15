bf2a3fa712afe458f152c32a6dee1f9d60b3200e
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-15 10:00:43 +0530

    Time: 615 ms (69.23%), Space: 19.6 MB (89.78%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -1,23 +1,30 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        fives=0
-        tens=0
-        twos=0
-        for i in bills:
-            if i==5:
-                fives+=1
-            elif i==10:
-                tens+=1
-                fives-=1
-                if fives<0:
-                    return False
-            else:
-                twos+=1
-                if tens>=1 and fives>=1:
-                    tens-=1
-                    fives-=1
-                elif tens<1 and fives>=3:
-                    fives-=3
+        five = 0
+        ten = 0
+
+        for bill in bills:
+            # if we get 5$
+            if bill == 5:
+                five += 1
+
+            elif bill == 10:
+                if five >= 1:
+                    five -= 1
+                    ten += 1
+
                 else:
                     return False
+
+            else:
+                if five >= 1 and ten >= 1:
+                    five -= 1
+                    ten -= 1
+
+                elif five >= 3:
+                    five -= 3
+
+                else:
+                    return False
+
         return True
