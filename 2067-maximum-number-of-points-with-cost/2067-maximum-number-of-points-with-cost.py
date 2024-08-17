4390d9716ad872a4734a208411280fd6471bf1ff
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-17 12:01:24 +0530

    Time: 1551 ms (52.68%), Space: 51.1 MB (15.61%) - LeetHub

---
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        m=len(points)
+        n=len(points[0])
+        dp=points[0]
+        left=[0]*n
+        right=[0]*n
+        for r in range(1, m):
+            for c in range(n):
+                if c == 0:
+                    left[c] = dp[c]
+                else:
+                    left[c] = max(left[c - 1] - 1, dp[c])
+            
+            for c in range(n - 1, -1, -1):
+                if c == n-1:
+                    right[c] = dp[c]
+                else:
+                    right[c] = max(right[c + 1] - 1, dp[c])
+                    
+            for c in range(n):
+                dp[c] = points[r][c] + max(left[c], right[c])
+                
+        return max(dp)
