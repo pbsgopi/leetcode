7cf21053c2b3723dade823de171c4dc806811f80
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-17 12:03:00 +0530

    Time: 1325 ms (98.05%), Space: 50.2 MB (74.63%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
@@ -1,24 +1,24 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        m=len(points)
-        n=len(points[0])
-        dp=points[0]
-        left=[0]*n
-        right=[0]*n
-        for r in range(1, m):
-            for c in range(n):
-                if c == 0:
-                    left[c] = dp[c]
+        m = len(points)
+        n = len(points[0])
+        dp = [0] * n
+        for i in range(m):
+            newDp = [0] * n
+            for j in range(n):
+                point = points[i][j]
+                if i > 0:
+                    point += dp[j]
+                if j > 0:
+                    if point > newDp[j - 1] - 1:
+                        k = j
+                        while k >= 0 and newDp[k] < point:
+                            newDp[k] = point
+                            k -= 1
+                            point -= 1
+                    else:
+                        newDp[j] = newDp[j - 1] - 1
                 else:
-                    left[c] = max(left[c - 1] - 1, dp[c])
-            
-            for c in range(n - 1, -1, -1):
-                if c == n-1:
-                    right[c] = dp[c]
-                else:
-                    right[c] = max(right[c + 1] - 1, dp[c])
-                    
-            for c in range(n):
-                dp[c] = points[r][c] + max(left[c], right[c])
-                
+                    newDp[j] = point
+            dp = newDp
         return max(dp)
