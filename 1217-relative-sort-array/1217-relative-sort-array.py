4ae63133edbe813c844fee8d7147a55f1cf1b82b
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-21 15:51:27 +0530

    Time: 34 ms (90.26%), Space: 16.9 MB (5.66%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,11 +1,15 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        count=collections.Counter(arr1)
+        dic={}
+        for i in arr1:
+            if dic.get(i) is None:
+                dic[i]=1
+            else:
+                dic[i]+=1
         res=[]
-        for i in arr2:
-            for _ in range(count.pop(i)):
-                res.append(i)
-        for i in range(min(arr1),max(arr1)+1):
-            for _ in range(count.pop(i,0)):
-                res.append(i)
-        return res
+        for j in arr2:
+            res+=[j]*dic[j]
+        extra=[]
+        for k in set(arr1)-set(arr2):
+            extra+=[k]*dic[k]
+        return res+sorted(extra)
