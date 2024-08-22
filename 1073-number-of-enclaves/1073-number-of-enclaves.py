2863af51ea587aafc3f7d74dc28ef6ae241520be
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-22 15:47:12 +0530

    Time: 449 ms (85.06%), Space: 27.9 MB (54.61%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -1,13 +1,22 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        def dfs(i, j):
-            grid[i][j] = 0
-            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
-                if 0 <= x < m and 0 <= y < n and grid[x][y]:
-                    dfs(x, y)
-        m, n = len(grid), len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1 and (i == 0 or j == 0 or i == m - 1 or j == n - 1):
-                    dfs(i, j)
-        return sum(sum(row) for row in grid)
+        n=len(grid)
+        m=len(grid[0])
+        def dfs(i,j):
+            if i==-1 or j==-1 or i==n or j==m or grid[i][j]==0:
+                return 
+            grid[i][j]=0
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+
+        for i in range(n):
+            for j in range(m):
+                if (i==0 or j==0 or n-1==i or m-1==j) and grid[i][j]==1:
+                    dfs(i,j)
+        s=0
+        for i in range(n):
+            for j in range(m):
+                s+=grid[i][j]
+        return s
