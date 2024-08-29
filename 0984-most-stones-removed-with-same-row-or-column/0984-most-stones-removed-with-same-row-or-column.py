2260f377bbf15e14f6ef195357e21200cb1434f5
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-08-29 10:12:04 +0530

    Time: 121 ms (83.03%), Space: 17.1 MB (96.85%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -1,19 +1,45 @@
-class Solution(object):
-    def dfs(self, n, idx, visited, stones):
-        visited[idx] = True
-        for i in range(n):
-            if not visited[i]:
-                if stones[idx][0] == stones[i][0]:
-                    self.dfs(n, i, visited, stones)
-                if stones[idx][1] == stones[i][1]:
-                    self.dfs(n, i, visited, stones)
+class Solution:
+    def __init__(self):
+        self.par = []
+        self.rank = []
+
+    def find_parent(self, x):
+        if x == self.par[x]:
+            return x
+        self.par[x] = self.find_parent(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y):
+        par_x = self.find_parent(x)
+        par_y = self.find_parent(y)
+
+        if par_x == par_y:
+            return False
+
+        if self.rank[par_x] < self.rank[par_y]:
+            par_x, par_y = par_y, par_x
+
+        self.rank[par_x] += self.rank[par_y]
+        self.par[par_y] = par_x
+        return True
+
     def removeStones(self, stones):
         n = len(stones)
-        group = 0
-        visited = [False] * n
+        self.par = [i for i in range(n)]
+        self.rank = [1 for i in range(n)]
+        X = {}
+        Y = {}
+        count = 0
 
-        for i in range(n):
-            if not visited[i]:
-                group += 1
-                self.dfs(n, i, visited, stones)
-        return n - group 
+        for i, (x, y) in enumerate(stones):
+            if x in X:
+                count += self.union(i, X[x])
+            else:
+                X[x] = i
+
+            if y in Y:
+                count += self.union(i, Y[y])
+            else:
+                Y[y] = i
+
+        return count
