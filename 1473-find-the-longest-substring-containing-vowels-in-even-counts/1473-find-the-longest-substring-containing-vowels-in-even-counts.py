e068e16528138271dc38a0c9cbeb820a95354c12
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-09-15 11:22:55 +0530

    Time: 290 ms (76.04%), Space: 22.2 MB (33.15%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,21 +1,16 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        vowels = ['a','e','i','o','u']
-        bitmask = [0] * 5
-        firstoccurance = {0:-1}
+        vowel = {"a": 1, "e": 2, "i": 4, "o": 8, "u": 16}
+        memo = {0: -1}
+        mask = 0 
         result = 0
-        for i in range(len(s)):
-            if s[i] in vowels:
-                index = vowels.index(s[i])
-                if bitmask[index] == 0:
-                    bitmask[index] = 1
-                else:
-                    bitmask[index] = 0
-            total = 0
-            for j in range(5):
-                total = total + bitmask[j]*(2**j)
-            if total in firstoccurance:
-                result = max(result, i-firstoccurance[total])
+        n = len(s)
+        for i in range(n):            
+            c = s[i]
+            if c in vowel:
+                mask = mask ^ vowel[c]
+            if mask not in memo:
+                memo[mask] = i
             else:
-                firstoccurance[total] = i
+                result = max(result, i - memo[mask])
         return result
