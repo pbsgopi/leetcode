2b84328638f9ab7332bb378a082c8508cb089b82
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-09-16 10:33:22 +0530

    Time: 86 ms (8.29%), Space: 19.6 MB (36.73%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -1,12 +1,10 @@
 class Solution:
     def findMinDifference(self, time: List[str]) -> int:
-        for i in range(len(time)):
-            time[i]=time[i].split(':')
-            time[i]=[int(time[i][0]),int(time[i][1])]
-
-        mini=float('inf')
+        def minute(a):
+            return int(a[:2]) * 60 + int(a[-2:])
         time.sort()
-        for i in range(1,len(time)):
-            mini=min(mini,(time[i][0]-time[i-1][0])*60 +(time[i][1]-time[i-1][1]))
-        mini=min(mini,(time[0][0]+23-time[-1][0])*60 +(time[0][1]+60-time[-1][1]))
-        return mini
+        ans = 12 * 60
+        for i in range(len(time)):
+            j = (i + 1) % len(time)
+            ans = min(ans, (minute(time[j]) - minute(time[i])) % 1440)
+        return ans
