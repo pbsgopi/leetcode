b105cf990adb6f871db2e842612990638e2c4075
pbsgopi <116747471+pbsgopi@users.noreply.github.com>
2024-12-26 14:04:16 +0530

    Time: 17 ms (98.63%), Space: 17.7 MB (66.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,11 +1,24 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp={}
-        def backtrack(i,total):
-            if i==len(nums):
-                return 1 if total==target else 0
-            if (i,total) in dp:
-                return dp[(i,total)]
-            dp[(i,total)]=(backtrack(i+1,total+nums[i])+backtrack(i+1,total-nums[i]))
-            return dp[(i,total)]
-        return backtrack(0,0)
+        total = 0
+        for num in nums:
+            total += num
+        
+        if total < target:
+            return 0
+        
+        target = (total - target)
+        
+        if target % 2 == 1:
+            return 0
+        
+        target = target // 2
+
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] + dp[i - num]
+        
+        return dp[target]
